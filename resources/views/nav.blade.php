733d899101afacebce97492910f52ec7a5aa14e1
andres-martin-c <172848384+andres-martin-C@users.noreply.github.com>
2025-01-31 21:17:56 -0500

    Ajustes del navbar y section de garage

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -15,4 +15,50 @@
                 </svg>LUN - SAB: <time datetime="09:00">9:00 AM</time> - <time datetime="08:00">8:00 AM</time></p>
         </div>
     </nav>
+     <!-- Menu -->
+     <nav class="flex items-center justify-between mx-4" x-data="{ open: false }" @click.away="open = false">
+        <!-- Logo -->
+        <img src="{{ asset('img/logoDinamo.png')}}" alt="logo-dinamo" class="size-16 ">
+
+        <!-- Botón de menú hamburguesa (solo en pantallas pequeñas) -->
+        <button class="lg:hidden text-gray-700 hover:text-gray-900 focus:outline-none cursor-pointer bg-gray-300 rounded" x-on:click="open = ! open">
+            <svg class="size-10" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+            </svg>
+        </button>
+
+        <!-- Menú (oculto en pantallas grandes, visible en pantallas pequeñas) -->
+        <div class="hidden lg:flex space-x-6">
+            <a href="#" class="text-gray-700 hover:text-gray-900">Inicio</a>
+            <a href="#" class="text-gray-700 hover:text-gray-900">Servicios</a>
+            <a href="#" class="text-gray-700 hover:text-gray-900">Contacto</a>
+        </div>
+
+        <!-- Menú para pantallas pequeñas -->
+        <div
+            x-show="open"
+            x-transition:enter="transition transform ease-out duration-300"
+            x-transition:enter-start="transform -translate-y-full"
+            x-transition:enter-end="transform translate-y-0"
+            x-transition:leave="transition transform ease-in duration-300"
+            x-transition:leave-start="transform translate-y-0"
+            x-transition:leave-end="transform -translate-y-full"
+            class="lg:hidden bg-gray-100 absolute top-0 right-0 w-full h-screen p-6 z-30">
+
+            <!-- Botón "Salir" en la parte superior derecha -->
+            <div class="absolute top-4 right-4">
+                <button class="text-gray-700 hover:text-gray-900 cursor-pointer px-2" x-on:click="open = false">
+                    <!-- Ícono de una X (tacha) -->
+                    <svg class="size-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+                    </svg>
+                </button>
+            </div>
+
+            <!-- Opciones de navegación -->
+            <a href="#" class="block py-2 text-gray-700 hover:text-gray-900">Inicio</a>
+            <a href="#" class="block py-2 text-gray-700 hover:text-gray-900">Servicios</a>
+            <a href="#" class="block py-2 text-gray-700 hover:text-gray-900">Contacto</a>
+        </div>
+    </nav>
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,64 +8,43 @@
 
 </head>
 
-<body class="">
+<body class="bg-white">
     <!-- header -->
     @include('nav')
-    <!-- Menu -->
-    <nav class="flex items-center justify-between mx-2 py-4" x-data="{ open: false }" @click.away="open = false">
-        <!-- Logo -->
-        <img src="{{ asset('img/logoDinamo.png')}}" alt="logo-dinamo" class="size-11 ">
-
-        <!-- Botón de menú hamburguesa (solo en pantallas pequeñas) -->
-        <button class="lg:hidden text-gray-700 hover:text-gray-900 focus:outline-none cursor-pointer bg-gray-300 rounded" x-on:click="open = ! open">
-            <svg class="size-7" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
-            </svg>
-        </button>
-
-        <!-- Menú (oculto en pantallas grandes, visible en pantallas pequeñas) -->
-        <div class="hidden lg:flex space-x-6">
-            <a href="#" class="text-gray-700 hover:text-gray-900">Inicio</a>
-            <a href="#" class="text-gray-700 hover:text-gray-900">Servicios</a>
-            <a href="#" class="text-gray-700 hover:text-gray-900">Contacto</a>
-        </div>
-
-        <!-- Menú para pantallas pequeñas -->
-        <div
-            x-show="open"
-            x-transition:enter="transition transform ease-out duration-300"
-            x-transition:enter-start="transform -translate-y-full"
-            x-transition:enter-end="transform translate-y-0"
-            x-transition:leave="transition transform ease-in duration-300"
-            x-transition:leave-start="transform translate-y-0"
-            x-transition:leave-end="transform -translate-y-full"
-            class="lg:hidden bg-gray-100 absolute top-0 right-0 w-full h-screen p-6">
-
-            <!-- Botón "Salir" en la parte superior derecha -->
-            <div class="absolute top-4 right-4">
-                <button class="text-gray-700 hover:text-gray-900 cursor-pointer px-2" x-on:click="open = false">
-                    <!-- Ícono de una X (tacha) -->
-                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
-                    </svg>
-                </button>
-            </div>
-
-            <!-- Opciones de navegación -->
-            <a href="#" class="block py-2 text-gray-700 hover:text-gray-900">Inicio</a>
-            <a href="#" class="block py-2 text-gray-700 hover:text-gray-900">Servicios</a>
-            <a href="#" class="block py-2 text-gray-700 hover:text-gray-900">Contacto</a>
-        </div>
-    </nav>
-
-    <div></div>
     <!-- Carrousel -->
-    <div></div>
+
     <!-- Cuerpo de la pagina -->
     <main>
-        <div>
+        <!-- Sección de que es esa empresa -->
+        <section>
+            <div class="flex justify-center gap-x-1 items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                    <path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
+                    <path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
+                </svg>
 
-        </div>
+                <h3 class="font-bold text-2xl text-center">¿Quiénes somos?</h3>
+            </div>
+            <!-- Texto del contenido -->
+            <p class="p-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil,
+                quas adipisci molestias nesciunt eligendi cumque iusto accusantium omnis
+                qui minima iure obcaecati officia est ipsam reprehenderit laborum consequuntur aspernatur odio.
+            </p>
+        </section>
+        <!-- Sección de categorías de motos -->
+        <section class="p-3 bg-[#F5F5F5] relative">
+            <h3 class="font-black text-5xl text-center pb-4"> Garage </h3>
+            <!-- Contenedor de las categorías. -->
+            <div class="grid-rows-1">
+                 <!--  ! Resolver pug -->
+                <!-- Se necesita poner group -->
+                <div class="group bg-cover bg-center h-64 relative flex items-center justify-center shadow-lg filter brightness-50" style="background-image: url('https://images.unsplash.com/photo-1735252723552-138dc3fb6f14?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');">
+                    <span class="font-bold text-white bottom-2 text-center text-4xl ">Trabajo</span>
+                    <button class="absolute group-hover:block hidden bottom-4 left-1/2 transform -translate-x-1/2 bg-white text-black px-4 py-2 rounded-lg opacity-75 hover:opacity-100 transition">Saber más</button>
+                </div>
+
+            </div>
+        </section>
     </main>
 </body>
 
